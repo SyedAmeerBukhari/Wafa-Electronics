73c9a4b3d7d27b588d2051abfb88bcc1029df3f5
SyedAmeerBukhari <155460572+SyedAmeerBukhari@users.noreply.github.com>
2024-10-12 08:51:26 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const ctaButton = document.querySelector(".cta-button");
+    
+    ctaButton.addEventListener("click", () => {
+        alert("Welcome to ElectroShop! Browse our products.");
+    });
+});
